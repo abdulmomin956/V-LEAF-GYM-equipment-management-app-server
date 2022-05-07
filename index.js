55f0676b783a1b4f4d78045bf02efdbc15ca011c
Abdul <abdulmomin956@gmail.com>
2022-05-07 20:11:45 +0600

    mongo connect

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 const app = express();
+
+
+
+const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@products.hm5pg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("gleaf").collection("products");
+    // perform actions on the collection object
+    client.close();
+});
+
 
 app.get('/', (req, res) => {
     res.send('Running the server')
