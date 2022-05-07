7b5d6b8d48d7b74a57405118b4c1870366d49289
Abdul <abdulmomin956@gmail.com>
2022-05-07 21:10:37 +0600

    function added

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
 const port = process.env.PORT || 5000;
 
 const app = express();
+
+app.use(express.json());
+app.use(cors());
 
 
 
@@ -14,6 +18,19 @@ client.connect(err => {
     // perform actions on the collection object
     client.close();
 });
+
+async function run() {
+    try {
+        await client.connect();
+        const productCollection = client.db("gleaf").collection("products");
+
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
